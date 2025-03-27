7eb2612cc674416dbca4cf642c2303d7dfb275a5
Felipe Fink Grael <felipe.grael@twistsystems.com>
2025-03-26 22:06:58 -0300

    Pipelines Implementados

--- src/titanic/pipelines/classifier/nodes.py
+++ src/titanic/pipelines/classifier/nodes.py
@@ -3,6 +3,7 @@ This is a boilerplate pipeline 'classifier'
 generated using Kedro 0.19.11
 """
 import pandas as pd
+from pycaret.classification import ClassificationExperiment
 
 
 def prepare_data(raw_data):
@@ -17,3 +18,35 @@ def prepare_data(raw_data):
         .dropna()
         .assign(Survived=lambda x: x['Survived'].astype(bool))
     )
+
+
+def best_model_from_comparison(data, session_id):
+
+    exp = ClassificationExperiment()
+    exp.setup(data=data, target='Survived', session_id=session_id)
+
+    best_model = exp.compare_models()
+
+    tuned_model = exp.tune_model(best_model, n_iter=10, optimize='AUC')
+
+    return tuned_model
+
+
+def logistic_regression_model(data, session_id):
+
+    exp = ClassificationExperiment()
+    exp.setup(data=data, target='Survived', session_id=session_id)
+
+    model = exp.create_model('lr')
+
+    tuned_model = exp.tune_model(model, n_iter=10, optimize='AUC')
+
+    return tuned_model
+
+
+def plot_roc(data, model, session_id, output_filename):
+
+    exp = ClassificationExperiment()
+    exp.setup(data=data, target='Survived', session_id=session_id)
+
+    exp.plot_model(model, plot='auc', save=output_filename)
--- src/titanic/pipelines/classifier/pipeline.py
+++ src/titanic/pipelines/classifier/pipeline.py
@@ -15,4 +15,44 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs='train',
             tags=['preprocessamento']
         ),
+        node(
+            nodes.logistic_regression_model,
+            inputs=[
+                'train',
+                'params:session_id',
+            ],
+            outputs='lr_model',
+            tags=['treinamento']
+        ),
+        # node(
+        #     nodes.best_model_from_comparison,
+        #     inputs=[
+        #         'train',
+        #         'params:session_id',
+        #     ],
+        #     outputs='best_model',
+        #     tags=['treinamento']
+        # ),
+        node(
+            nodes.plot_roc,
+            inputs=[
+                'train',
+                'lr_model',
+                'params:session_id',
+                'params:lr_model_roc_filename',
+            ],
+            outputs=None,
+            tags=['report']
+        ),
+        # node(
+        #     nodes.plot_roc,
+        #     inputs=[
+        #         'train',
+        #         'best_model',
+        #         'params:session_id',
+        #         'params:best_model_roc_filename',
+        #     ],
+        #     outputs=None,
+        #     tags=['report']
+        # ),
     ])
